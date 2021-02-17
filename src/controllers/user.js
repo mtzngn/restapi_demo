5068176e765577fce7a4422404e25faa69de9d1b
mtzngn <mt.zngn@gmail.com>
2021-02-17 11:29:57 +0000

    removed token from creatwe users respond

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -19,7 +19,7 @@ exports.createUser = async (req, res) => {
         const user = new User(req.body);
         const token = await user.generateAuthToken()
         const savedUser = await user.save();
-        res.status(201).send({savedUser, token});
+        res.status(201).send({savedUser});
     } catch (error) {
         if(error.code == 11000) {
             res.status(500).send({messages: "the email adress already exist!!"});
